ce2ab3cf93589b7482cb92f288a245ced5dd0735
MrazTevin <millatevin@gmail.com>
2023-09-13 23:41:27 +0300

    m: change colors fetch items from array

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -54,11 +54,11 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff040305),
+      backgroundColor: Color(0xffffffff),
       appBar: AppBar(
-        backgroundColor: Color(0xff040305),
+        backgroundColor: Color(0xffffffff),
         elevation: 0,
-        title: Text('About Me', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25)),
+        title: Text('About Me', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 25)),
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -72,7 +72,7 @@ class _HomePageState extends State<HomePage> {
                   // Your full name in a circular frame with rounded black border
                   CircleAvatar(
                     radius: 50.0, // Adjust the size as needed
-                    backgroundColor: Colors.black,
+                    backgroundColor: Colors.black, // Change as needed
                     child: CircleAvatar(
                       radius: 48.0, // Adjust the size as needed
                       backgroundImage: AssetImage('your_profile_image.png'), // Replace with your image asset
@@ -85,7 +85,7 @@ class _HomePageState extends State<HomePage> {
                     child: Text(
                       'Slack Username: Tevin Milla',
                       style: TextStyle(
-                        color: Colors.white,
+                        color: Colors.black,
                         fontSize: 18.0,
                       ),
                     ),
@@ -96,7 +96,7 @@ class _HomePageState extends State<HomePage> {
                     child: Text(
                       'GitHub: MrazTevin',
                       style: TextStyle(
-                        color: Colors.white,
+                        color: Colors.black,
                         fontSize: 18.0,
                       ),
                     ),
@@ -107,7 +107,7 @@ class _HomePageState extends State<HomePage> {
                     child: Text(
                       'I am interested in Mobile Development more than Web Development. I create visually aesthetic designs no matter what.',
                       style: TextStyle(
-                        color: Colors.white,
+                        color: Colors.black,
                         fontSize: 16.0,
                       ),
                     ),
@@ -122,7 +122,7 @@ class _HomePageState extends State<HomePage> {
                 padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
                 width: double.infinity,
                 decoration: BoxDecoration(
-                    color: Color(0xff1F1E25),
+                    color: Color(0xffEBE9E2), 
                     borderRadius: BorderRadius.circular(20.0)),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -147,57 +147,59 @@ class _HomePageState extends State<HomePage> {
                     Container(
                       padding: EdgeInsets.all(15.0),
                       decoration: BoxDecoration(
-                          color: Color(0xff1F1E25),
+                          color: Color(0xffEBE9E2),
                           borderRadius: BorderRadius.circular(20.0)),
                       // child: Text('Fullstack Developer', style: TextStyle(color: Colors.white, fontSize: 15)),
-                      child: Text(intrestss[0], style: TextStyle(color: Colors.white, fontSize: 15)),
+                      child: Text(intrestss[0], style: TextStyle(color: Colors.black, fontSize: 15)),
                     ),
                     SizedBox(width: 10.0),
                     Container(
                       padding: EdgeInsets.all(15.0),
                       decoration: BoxDecoration(
-                          color: Color(0xff1F1E25),
+                          color: Color(0xffEBE9E2),
                           borderRadius: BorderRadius.circular(20.0)),
                       // child: Text('Fullstack Developer', style: TextStyle(color: Colors.white, fontSize: 15)),
-                      child: Text(intrestss[1], style: TextStyle(color: Colors.white, fontSize: 15)),
+                      child: Text(intrestss[1], style: TextStyle(color: Colors.black, fontSize: 15)),
                     )
                   ])
                 ],
               ),
 
               SizedBox(height: 15.0),
-              Text('Education', style: TextStyle(color: Colors.white, fontSize: 18)),
+              Text('Education', style: TextStyle(color: Colors.black, fontSize: 18)),
               SizedBox(height: 15.0),
               // CardCustom(text: this.cvData['education'],colorIcon: Color(0xffA36FF6), isEducation: true, education: '2014 - 2016 . University',),
-              // CardCustom(text: 'Master in Backend Web',colorIcon: Color(0xffA36FF6), isEducation: true, education: '2014 - 2016 . University',),
-              // CardCustom(text: 'Master in Laravel',colorIcon: Color(0xffA36FF6), isEducation: true, education: '2016 - 2018 . University',),
-              // CardCustom(text: 'Bachiller in Sistemas',colorIcon: Color(0xffA36FF6), isEducation: true, education: '2019 - 2020 . University',),
+              CardCustom(text: educations[0],colorIcon: Color(0xffA36FF6), isEducation: true, education: '2014 - 2016 . University',),
+              CardCustom(text: educations[1],colorIcon: Color(0xffA36FF6), isEducation: true, education: '2016 - 2018 . University',),
+              CardCustom(text: educations[2],colorIcon: Color(0xffA36FF6), isEducation: true, education: '2019 - 2020 . University',),
               // CardCustom(text: education, colorIcon: Color(0xffA36FF6), isEducation: true, education: '2019 - 2020 . University',),
-              SingleChildScrollView(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: educations.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return CardCustom(text: educations[index], colorIcon: Color(0xffA36FF6), isEducation: true);
-                  },
-                ),
-              ),
+              // SingleChildScrollView(
+              //   child: ListView.builder(
+              //     shrinkWrap: true,
+              //     itemCount: educations.length,
+              //     itemBuilder: (BuildContext context, int index) {
+              //       return CardCustom(text: educations[index], colorIcon: Color(0xffA36FF6), isEducation: true);
+              //     },
+              //   ),
+              // ),
               SizedBox(height: 15.0),
-              Text('Experience', style: TextStyle(color: Colors.white, fontSize: 18)),
+              Text('Experience', style: TextStyle(color: Colors.black, fontSize: 18)),
               SizedBox(height: 15.0),
               // CardCustom(text: cvData['experience'], colorIcon: Color(0xffA36FF6), isEducation: false),
-              SingleChildScrollView(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: experiences.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return CardCustom(text: experiences[index], colorIcon: Color(0xffA36FF6), isEducation: false);
-                  },
-                ),
-              ),
+              // Flexible(
+              //   // height: 100.0,
+              //   child: ListView.builder(
+              //     shrinkWrap: true,
+              //     itemCount: experiences.length,
+              //     itemBuilder: (BuildContext context, int index) {
+              //       return CardCustom(text: experiences[index], colorIcon: Color(0xffA36FF6), isEducation: false);
+              //     },
+              //   ),
+              // ),
 
-              // CardCustom(text: 'Freelance Web Designer',colorIcon: Color(0xffA36FF6), isEducation: false),
-              // CardCustom(text: 'Senior Web Developer',colorIcon: Color(0xff83DBC5), isEducation: false),
+              CardCustom(text: experiences[0],colorIcon: Color(0xffA36FF6), isEducation: false),
+              CardCustom(text: experiences[1],colorIcon: Color(0xff83DBC5), isEducation: false),
+              CardCustom(text: experiences[2],colorIcon: Color(0xff0385DC), isEducation: false),
               // CardCustom(text: 'Semi Senior Web Developer',colorIcon: Color(0xff0385DC), isEducation: false),
               // CardCustom(text: 'Junior Web Developer',colorIcon: Color(0xffE62755), isEducation: false),
               // CardCustom(text: 'Freelance App Flutter',colorIcon: Color(0xffF7605D), isEducation: false),
